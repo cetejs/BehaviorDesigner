31159d626e812a128672fb60c5d610f9a4b74f87
Xianjun Yin <yinxianjun@xmfunny.com>
2023-02-10 18:38:08 +0800

    add transform nodes

---
+++ Runtime/BuiltIn/Tasks/Unity/Transform/GetLocalRotation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace BehaviorDesigner.Tasks.UnityTransform
+{
+    [TaskCategory("Transform")]
+    [TaskDescription("Stores the local rotation of the Transform. Returns Success.")]
+    public class GetLocalRotation : Action
+    {
+        [SerializeField]
+        private SharedTransform target;
+        [SerializeField] [RequiredField]
+        private SharedQuaternion storeResult;
+
+        private Transform Target
+        {
+            get { return target.Value ? target.Value : transform; }
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            storeResult.Value = Target.localRotation;
+            return TaskStatus.Success;
+        }
+
+        public override void OnReset()
+        {
+            target = null;
+            storeResult = Quaternion.identity;
+        }
+    }
+}
