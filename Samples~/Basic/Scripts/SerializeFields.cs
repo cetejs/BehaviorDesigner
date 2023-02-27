03a944962c2c5c5374fdd0b8eb0ddfe3e1272a60
Xianjun Yin <yinxianjun@xmfunny.com>
2023-02-27 15:15:17 +0800

    update samples

--- Samples~/Basic/Scripts/SerializeFields.cs
+++ Samples~/Basic/Scripts/SerializeFields.cs
@@ -29,18 +29,39 @@ public class SerializeFields : Action
     public Vector3Int v121;
     public Vector4 v122;
 
+    public List<AnimationCurve> v201;
+    public List<Behaviour> v202;
+    public List<bool> v203;
+    public List<Color> v204;
+    public List<double> v205;
+    public List<float> v206;
+    public List<GameObject> v207;
+    public List<int> v208;
+    public List<long> v209;
+    public List<Material> v210;
+    public List<Object> v211;
+    public List<Quaternion> v212;
+    public List<Rect> v213;
+    public List<string> v214;
+    public List<Transform> v215;
+    public List<Vector2> v216;
+    public List<Vector2Int> v217;
+    public List<Vector3> v218;
+    public List<Vector3Int> v219;
+    public List<Vector4> v220;
+
     public override void OnStart()
     {
         Debug.Log("AnimationCurve : " + v101);
         Debug.Log("Behaviour : " + v102);
-        Debug.Log("bool : " + v103);
+        Debug.Log("Bool : " + v103);
         Debug.Log("Color : " + v104);
-        Debug.Log("double : " + v105);
-        Debug.Log("float : " + v106);
+        Debug.Log("Double : " + v105);
+        Debug.Log("Float : " + v106);
         Debug.Log("GameObject : " + v107);
-        Debug.Log("int : " + v108);
+        Debug.Log("Int : " + v108);
         Debug.Log("LayerMask : " + v109);
-        Debug.Log("long : " + v110);
+        Debug.Log("Long : " + v110);
         Debug.Log("Material : " + v111);
         Debug.Log("Object : " + v112);
         Debug.Log("Quaternion : " + v113);
@@ -53,5 +74,26 @@ public class SerializeFields : Action
         Debug.Log("Vector3 : " + v120);
         Debug.Log("Vector3Int : " + v121);
         Debug.Log("Vector4 : " + v122);
+        
+        Debug.Log("AnimationCurveList : " + string.Join(",", v201));
+        Debug.Log("BehaviourList : " + string.Join(",", v202));
+        Debug.Log("BoolList : " + string.Join(",", v203));
+        Debug.Log("ColorList : " + string.Join(",", v204));
+        Debug.Log("DoubleList : " + string.Join(",", v205));
+        Debug.Log("FloatList : " + string.Join(",", v206));
+        Debug.Log("GameObjectList : " + string.Join(",", v207));
+        Debug.Log("IntList : " + string.Join(",", v208));
+        Debug.Log("LongList : " + string.Join(",", v209));
+        Debug.Log("MaterialList : " + string.Join(",", v210));
+        Debug.Log("ObjectList : " + string.Join(",", v211));
+        Debug.Log("QuaternionList : " + string.Join(",", v212));
+        Debug.Log("RectList : " + string.Join(",", v213));
+        Debug.Log("StringList : " + string.Join(",", v214));
+        Debug.Log("TransformList : " + string.Join(",", v215));
+        Debug.Log("Vector2List : " + string.Join(",", v216));
+        Debug.Log("Vector2IntList : " + string.Join(",", v217));
+        Debug.Log("Vector3List : " + string.Join(",", v218));
+        Debug.Log("Vector3IntList : " + string.Join(",", v219));
+        Debug.Log("Vector4List : " + string.Join(",", v220));
     }
 }
